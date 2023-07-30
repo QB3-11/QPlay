985ddc7a14cc3971cbb50a9c139db6fde08aa1fe
Abhay Kagdiyal <110809881+QB3-11@users.noreply.github.com>
2023-07-30 12:25:49 +0530

    Add git.py
    Downloads music from youtube

---
+++ get.py
@@ -0,0 +1,18 @@
+from pytube import YouTube
+from sys import argv
+
+defaultpath = "//home//qb3//Music"
+
+def download(url, title = None, path = defaultpath):
+    video = YouTube(url)
+    file = video.streams.get_by_itag(251)
+    file.download(output_path=path, filename=title)
+
+if len(argv) == 3:
+    download(argv[1], argv[2])
+
+elif len(argv) == 4:
+    download(argv[1], argv[2], argv[3])
+
+else:
+    download(argv[1])
